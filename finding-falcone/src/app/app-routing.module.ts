da2cf3662f1b4f29eeba8dd15524da692ea6861e
lokesh p <lokeshpatidar63@gmail.com>
2021-05-22 19:42:05 +0000

    basic structure and routes

--- finding-falcone/src/app/app-routing.module.ts
+++ finding-falcone/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ResultComponent } from './components/result/result.component';
+import { SelectionComponent } from './components/selection/selection.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'selection', pathMatch: 'full' },
+  { path: 'selection', component: SelectionComponent },
+  { path: 'result', component: ResultComponent },
+  { path: '**', component: SelectionComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
